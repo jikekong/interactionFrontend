a5beb8d0520e9c31e85b571072524c365e69ad2f
Chris1123F <1040701709@qq.com>
2020-03-28 15:05:47 +0800

    还剩 后端接口 smartgesture 几个方法

--- interaction/src/components/HelloWorld.vue
+++ interaction/src/components/HelloWorld.vue
@@ -1,113 +1,182 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div class="configure">
+    <h2>配置 configuration</h2>
+    <p><input type="checkbox"
+              id="path"
+              onchange="renewGesture()"
+              checked><label for="path">开启鼠标轨迹</label><span class="option-tip">enablePath</span></p>
+    <p><span class="config-title">触发手势识别时间(ms)</span><input id="delay"
+                                                            type="number"
+                                                            value="0"
+                                                            onchange="renewGesture()"><span class="option-tip">timeDelay</span></p>
+    <p><span class="config-title">鼠标轨迹颜色</span><input id="color"
+                                                      type="text"
+                                                      value="#666"
+                                                      onchange="renewGesture()"><span class="option-tip">lineColor</span></p>
+    <p><span class="config-title">鼠标轨迹宽度</span><input id="width" type="text" value="4" onchange="renewGesture()"><span
+      class="option-tip">lineWidth</span></p>
+    <p><span class="config-title">触发手势识别鼠标按键</span><input id="key"
+                                                          type="text"
+                                                          value="left"
+                                                          onchange="renewGesture()"><span class="option-tip">triggerMouseKey</span></p>
+    <p><span class="config-title">开启手势时背景色</span><input id="active-color"
+                                                        type="text"
+                                                        value="rgba(0, 0, 0, .05)"
+                                                        onchange="renewGesture()"><span class="option-tip">triggerMouseKey</span></p>
+
+  </div>
+    <div class="recognize">
+    <div class="add">
+      <h2>手势区域</h2>
+      <label>添加新手势 </label>
+      <input id="gestureName" placeholder="手势名称">
+      <button id="btn" onclick="addGesture()">ADD</button>
+    </div>
+    <div class="result">
+      <h2>识别结果</h2>
+      <p>Gesture result: <span style="font-weight: bold; color: #2b5;" id="result"></span></p>
+      <p>Swipe directions result: <span style="font-weight: bold; color: #2b5;" id="result0"></span></p>
+    </div>
+    <div id="test" class="stage">
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
+  components: {
+  },
   data () {
     return {
+      lastPoints: [],
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  renewGesture () {
+    var options = {
+      enablePath: document.getElementById('path').checked,
+      timeDelay: document.getElementById('delay').value,
+      lineColor: document.getElementById('color').value,
+      lineWidth: document.getElementById('width').value,
+      triggerMouseKey: document.getElementById('key').value,
+      activeColor: document.getElementById('active-color').value,
+    }
+    var canvas = new SmartGesture(options)
+    canvas.refresh(options)
+  },
+  mounted () {
+
+
+    var options = {
+      el: document.getElementById('test'),
+      enablePath: document.getElementById('path').checked,
+      timeDelay: document.getElementById('delay').value,
+      lineColor: document.getElementById('color').value,
+      lineWidth: document.getElementById('width').value,
+      triggerMouseKey: document.getElementById('key').value,
+      activeColor: document.getElementById('active-color').value,
+      onSwipe: (list) => {
+        document.getElementById('result0').innerHTML = list.join('');
+        console.log(list)
+      },
+      onGesture: (res, points) => {
+
+        //此处调用后端接口
+        console.log(res);
+        document.getElementById('result').innerHTML = res.score > 2 ? res.name : '未识别'
+        this.data.lastPoints = points
+      }
+    }
+
+    var canvas = new SmartGesture(options)
+
+    document.getElementById('btn').addEventListener('click', () => {
+      canvas.addGesture({
+        name: document.getElementById('gestureName').value,
+        points: lastPoints
+      })
+      document.getElementById('gestureName').value = ''
+    })
+  },
+  addGesture () {
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .configure {
+    position: relative;
+  }
+
+  .configure,
+  .recognize {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 0 15px;
+    margin-top: 10px;
+  }
+
+  .add,
+  .result {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  .result {
+    margin-left: 100px;
+    padding-left: 100px;
+    border-left: 1px dashed #ccc;
+  }
+
+  .config-title {
+    display: inline-block;
+    width: 200px;
+  }
+
+  .option-tip {
+    margin-left: 10px;
+    font-size: 12px;
+    vertical-align: middle;
+  }
+
+  .stage {
+    height: 400px;
+    background: #ddd;
+    position: relative;
+    margin: 15px 0;
+  }
+
+  .stage:before {
+    content: '绘制区域';
+    color: #eeeeee;
+    font-size: 30px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .build-in {
+    position: absolute;
+    width: 400px;
+    right: 250px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .build-in:before {
+    content: '内置图形';
+    position: absolute;
+    margin-top: -1.4em;
+  }
 </style>
--- interaction/src/main.js
+++ interaction/src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import SmartGesture from 'smart-gesture'
 import App from './App'
 import router from './router'
+Vue.use(SmartGesture)
 
 Vue.config.productionTip = false
 
